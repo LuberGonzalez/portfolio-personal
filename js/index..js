a0192e791d3ebfefe7ee3ed7e6e808336b920976
lubergonzalez <luber.gonzalez.77@gmail.com>
2023-08-22 21:43:49 +0200

    Se inicio la parte del havascript con prevencion de erroes, para mostrar un efecto, a los enlaces del navbar, dependiendo de donde este sea activa dicho elemento, ademas se corrigieron errores tanto en el html como en el css, se sigue haciendo la aplicacion mas robusta para prevenir errores.

--- js/index..js
+++ js/index..js
@@ -1,21 +1,57 @@
 // toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
+const $menuIcon = document.querySelector("#menu-icon");
+const $navbar = document.querySelector(".navbar");
 
 // scroll sections
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+const $sections = document.querySelectorAll(".section");
+const $navLinks = document.querySelectorAll(".main-menu-link");
 
 window.onscroll = () => {
+  // Sticky header
+  const $header = document.querySelector(".main-header");
+ 
+  if($header) {
+    $header.classList.toggle("sticky", window.scrollY > 100);
+  }else {
+    console.error("NO existe el Header");
+  }
 
+  // Remove toggle icon and navbar when clicking navbar links (scroll)
 
-    // sticky header
+  if ($sections.length > 0) {
+    $sections.forEach((sec) => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 100;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute("id");
 
+      if (top >= offset && top < offset + height) {
+        // active navbar links
 
-    // remove toggle icon and navbar when click navbar links (scroll)
+        if ($navLinks.length > 0) {
+          $navLinks.forEach((link) => {
+            link.classList.remove("active");
+            const linkToActivate = document.querySelector(
+              `.main-menu-link[href*=${id}]`
+            );
+            if (linkToActivate) {
+              linkToActivate.classList.add("active");
+            } else {
+              console.error(
+                `No se encontró un enlace para la sección con id ${id}`
+              );
+            }
+          });
+        } else {
+          console.error("No hay enlaces");
+        }
+      }
+    });
+  } else {
+    // Handle the case where no sections were found
 
+    console.error("No se hizo n");
+  }
 
-    // animation footer on scroll
-
-}
+  // Animation footer on scroll
+};
